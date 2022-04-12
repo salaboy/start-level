0ef14dfab658dfc5c30bad32e7151881d50a6e0d
salaboy <Salaboy@gmail.com>
2022-04-12 11:29:08 +0100

    first commit

---
+++ handle.go
@@ -0,0 +1,59 @@
+package function
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"github.com/go-redis/redis"
+	"time"
+)
+
+var redisHost = os.Getenv("REDIS_HOST")
+
+type GameTime struct{
+	GameTimeId string
+	SessionId string
+	Level string
+	Type string
+	Time      time.Time
+}
+
+// Handle an HTTP Request.
+func Handle(ctx context.Context, res http.ResponseWriter, req *http.Request) {
+	client := redis.NewClient(&redis.Options{
+		Addr:     redisHost + ":6379",
+		Password: "",
+		DB:       0,
+	})
+
+	var gt GameTime
+	// Try to decode the request body into the struct. If there is an error,
+	// respond to the client with the error message and a 400 status code.
+	err := json.NewDecoder(req.Body).Decode(&gt)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	gt.GameTimeId = "time-" + gt.SessionId
+	gt.Type = "start"
+	gt.Time = time.Now()
+
+	gameTimeJson, err := json.Marshal(gt)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err = client.RPush(gt.GameTimeId, string(gameTimeJson)).Err()
+	// if there has been an error setting the value
+	// handle the error
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintln(res, string(gameTimeJson))
+}
